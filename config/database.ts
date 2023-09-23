6662290e9afc8116e2fe7f441b11f608c49d7aed
Kristjan Jansen <kristjan.jansen@gmail.com>
2023-09-23 15:28:26 +0300

    init

---
+++ config/database.ts
@@ -0,0 +1,22 @@
+import { parse } from "pg-connection-string";
+
+export default ({ env }) => {
+  const config = parse(env("DATABASE_URL"));
+  return {
+    connection: {
+      client: "postgres",
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: config.ssl
+          ? {
+              rejectUnauthorized: false,
+            }
+          : null,
+      },
+    },
+  };
+};
