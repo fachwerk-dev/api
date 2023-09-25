0cc7c466a084c78ed4d689219970d9527ec4f5ad
Kristjan Jansen <kristjan.jansen@gmail.com>
2023-09-25 21:11:59 +0300

    fixes

--- config/server.ts
+++ config/server.ts
@@ -1,11 +1,9 @@
 export default ({ env }) => ({
+  proxy: true,
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
     keys: env.array("APP_KEYS"),
-  },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
   url: env("SERVER_URL", "http://localhost:1337"),
 });
