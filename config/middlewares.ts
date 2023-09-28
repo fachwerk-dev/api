a141cc6fa5fb64d5a25ef63ce5ad63923eae1078
Kristjan Jansen <kristjan.jansen@gmail.com>
2023-09-28 10:31:28 +0300

    image upload

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,26 @@
-export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", env("DO_SPACE_CDN")],
+          "media-src": ["'self'", "data:", "blob:", env("DO_SPACE_CDN")],
+          upgradeInsecureRequests: null,
+          frameAncestors: ["http://localhost:*", "self"],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,8 +1,21 @@
-export default () => ({
+export default ({ env }) => ({
   "users-permissions": {
     config: {
       jwt: {
         expiresIn: "30d",
+      },
+    },
+  },
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do",
+      providerOptions: {
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY"),
+        endpoint: "https://" + env("DO_SPACE_ENDPOINT"),
+        space: env("DO_SPACE_BUCKET"),
+        directory: env("DO_SPACE_DIRECTORY"),
+        cdn: env("DO_SPACE_CDN"),
       },
     },
   },
